823e9f3c83c5e0d5624eaeb313d9a24482735ed3
Junlei Chen <568538066@qq.com>
2019-07-08 18:02:56 +0800

    test event this

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 
 class Button extends React.Component {
+    constructor (props: Object) {
+        super(props);
+        this.state = {
+            isOn: false
+        }
+    }
+    showThis(p:string) {
+        console.log(this);
+        console.log(p)
+    }
     render() {
         return (
-            <h1>Hello, React</h1>
+            <h1 onClick={this.showThis.bind(this, '123')}>Hello</h1>
         )
     }
 }
